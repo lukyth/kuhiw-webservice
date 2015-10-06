191ba993539302261fcaafb74d490f092eeb2b65
Kanitkorn S <k.sujautra@gmail.com>
2015-10-06 12:59:25 +0700

    Add model Restaurant and rewrite restaurant route
    Use model to manage all data transaction.
    Scope route down to only know what appropriate data to response.

---
+++ models/Restaurant.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace models;
+
+use lib\Core;
+use PDO;
+
+class Restaurant {
+
+    protected $core;
+
+    function __construct() {
+        $this->core = Core::getInstance();
+        $this->core->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function getRestaurant($id) {
+        $sql = "SELECT * FROM restaurants WHERE id=:id";
+        try {
+            $stmt = $this->core->dbh->prepare($sql);
+            $stmt->bindParam("id", $id, PDO::PARAM_INT);
+            $stmt->execute();
+            $restaurant = $stmt->fetchObject();
+            return $restaurant;
+        } catch(PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function getRestaurants() {
+        $sql = "SELECT * FROM restaurants";
+        try {
+            $stmt = $this->core->dbh->prepare($sql);
+            $stmt->execute();
+            $restaurants = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $restaurants;
+        } catch(PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function getRestaurantOwners($id) {
+        $sql = "SELECT * FROM owners WHERE restaurant_id=:id";
+        try {
+            $stmt = $this->core->dbh->prepare($sql);
+            $stmt->bindParam("id", $id, PDO::PARAM_INT);
+            $stmt->execute();
+            $owner = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $owner;
+        } catch(PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+
+}
--- routes/restaurant.php
+++ routes/restaurant.php
@@ -1,35 +1,28 @@
 <?php
 
-use lib\Core;
+use models\Restaurant;
 
 $app->group('/restaurant', function () use ($app) {
 
-    $app->get('/:id', function ($id) {
-        echo json_encode([
-            'name' => 'restaurant_' . $id . '_name',
-            'latitude' => 'restaurant_' . $id . '_latitude',
-            'longitude' => 'restaurant_' . $id . '_longitude'
-        ]);
+    $app->get('/', function () use ($app) {
+        $model = new Restaurant ();
+        $restaurants = $model->getRestaurants();
+        $app->contentType('application/json');
+        echo json_encode($restaurants);
     });
 
-    $app->get('/menu/:id', function ($id) {
-        echo json_encode([
-            'name' => 'menu_' . $id . '_name',
-            'price' => 'menu_' . $id . '_price'
-        ]);
+    $app->get('/:id', function ($id) use ($app) {
+        $model = new Restaurant ();
+        $restaurant = $model->getRestaurant($id);
+        $app->contentType('application/json');
+        echo json_encode($restaurant);
     });
 
-    $app->get('/owner/:id', function ($id) {
-        $sql = "SELECT * FROM owners WHERE id=:id";
-        try {
-            $stmt = Core::getInstance()->dbh->prepare($sql);
-            $stmt->bindParam("id", $id);
-            $stmt->execute();
-            $owner = $stmt->fetchObject();
-            echo json_encode($owner);
-        } catch(PDOException $e) {
-            echo $e->getMessage();
-        }
+    $app->get('/:id/owner', function ($id) use ($app) {
+        $model = new Restaurant ();
+        $owners = $model->getRestaurantOwners($id);
+        $app->contentType('application/json');
+        echo json_encode($owners);
     });
 
 });
