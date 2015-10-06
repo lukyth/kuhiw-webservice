5e9c57ce646bfa7aba1b4a476a51d6e360451737
Kanitkorn S <k.sujautra@gmail.com>
2015-10-06 23:07:20 +0700

    Test API response using PHPUnit and Guzzle

---
+++ tests/RestaurantTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class ProgrammerControllerTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGetRestaurantShouldReturnAllRestaurantFields()
+    {
+        $client = new GuzzleHttp\Client();
+        $response = $client->get('http://localhost:8000/restaurant/1');
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $data = $response->json();
+
+        $this->assertArrayHasKey('id', $data);
+        $this->assertArrayHasKey('name', $data);
+    }
+}
