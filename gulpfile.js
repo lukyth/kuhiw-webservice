f399945c45bcb88cc1c4fa584eba39385e835570
Kanitkorn S <k.sujautra@gmail.com>
2015-10-21 06:05:51 +0700

    Rewrite gulpfile following the packages's doc

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,28 @@
-var gulp = require('gulp');
-var ftp = require('vinyl-ftp');
-var gutil = require('gulp-util');
-var minimist = require('minimist');
-var args = minimist(process.argv.slice(4));
+var gulp = require('gulp'),
+    ftp = require('vinyl-ftp'),
+    gutil = require('gulp-util'),
+    minimist = require('minimist');
+
+var args = minimist(process.argv.slice(2));
 
 gulp.task('deploy', function() {
-  var remotePath = args.path;
-  var conn = ftp.create({
-    host: args.host,
-    user: args.user,
-    password: args.password,
-    log: gutil.log
-  });
 
-  gulp.src(['*', '.*'])
-    .pipe(conn.newer(remotePath))
-    .pipe(conn.dest(remotePath));
-});
+  var remotePath = args.path
+
+  var conn = ftp.create({
+    host:     args.host,
+    user:     args.user,
+    password: args.password,
+    parallel: 10,
+    log:      gutil.log
+  })
+
+  var globs = [
+    '.*',
+    '**'
+  ]
+
+  return gulp.src(globs, {base: '.', buffer: false})
+        .pipe(conn.newer(remotePath)) // only upload newer files
+        .pipe(conn.dest(remotePath))
+})
